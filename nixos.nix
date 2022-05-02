fcf72a36f11671db169e2166ccc3f7fca528cadb
Arnaud Spiwack <arnaud@spiwack.net>
2022-05-02 13:56:13 +0200

    Add Dropbox

--- nixos.nix
+++ nixos.nix
@@ -7,4 +7,8 @@
   # paths it should manage.
   home.username = "aspiwack";
   home.homeDirectory = "/home/aspiwack";
+
+  # For now, let me enable Dropbox on Nixos only, as my non-Nixos
+  # computer have it enabled by more traditional means at the moment.
+  services.dropbox.enable = true;
 }
