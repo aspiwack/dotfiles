e67414bbae9cabe4eadab09c26a75a7be605cc8d
Arnaud Spiwack <arnaud@spiwack.net>
2023-06-01 16:46:30 +0200

    Nushell: use native home-manager integration for direnv

--- config/terminal.nix
+++ config/terminal.nix
@@ -39,18 +39,6 @@
       starship init nu | save ~/.cache/starship/init.nu
     '';
     configFile.text = ''
-      let-env config = {
-        hooks: {
-          pre_prompt: [{
-            code: "
-              let direnv = (direnv export json | from json)
-              let direnv = if ($direnv | length) == 1 { $direnv } else { {} }
-              $direnv | load-env
-            "
-          }]
-        }
-      }
-
       # Starship integration
       source ~/.cache/starship/init.nu
     '';
